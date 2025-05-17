771201eed345130955927762427e90dcb42eb6c8
aoi_takenaka <aaoooii219@gmail.com>
2025-05-17 10:23:53 +0000

    Fix: カラム設定修正

--- db/migrate/20250517101259_devise_create_customers.rb
+++ db/migrate/20250517101259_devise_create_customers.rb
@@ -40,7 +40,7 @@ class DeviseCreateCustomers < ActiveRecord::Migration[6.1]
       t.string :postal_code, null: false
       t.string :address, null: false
       t.string :telephone_number, null: false
-      t.boolean :is_active, null: false
+      t.boolean :is_active, null: false, default: true
 
       t.timestamps null: false
     end
