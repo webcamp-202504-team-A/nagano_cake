ab58da39cb8021631b3abfeb33bc409de7cff11e
aoi_takenaka <aaoooii219@gmail.com>
2025-05-18 06:38:51 +0000

    Update: 顧客側ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,41 @@ Rails.application.routes.draw do
     sessions: 'public/sessions'
   }
 
+  # get 'items' => 'public/items#index'
+    # resources :items, only: [:index]
+
+  # 会員側のルーティング設定
+  scope module: :public do
+    root to: "homes#top"
+    get "/about", to: "homes#about"
+  
+    resources :items, only: [:index, :show]
+
+    get "/customers/my_page", to: "customers#show"
+    get "/customers/information/edit", to: "customers#edit"
+    patch "/customers/information", to: "customers#update"
+    get "/customers/unsubscribe", to: "customers#unsubscribe"
+    patch "/customers/withdraw", to: "customers#withdraw"
+
+    resources :cart_items, only: [:index, :update, :destroy, :create]
+    delete "/cart_items/destroy_all", to: "cart_items#destroy_all"
+
+    resources :orders, only: [:index, :show, :new, :create]
+    post "/orders/confirm", to: "orders#confirm"
+    get "/orders/complete", to: "orders#complete"
+    
+    resources :addresses, only: [:index, :edit, :destroy, :create, :update]
+  end
+
   # 管理者用
   # URL /admin/sign_in ...
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
+
+  # 管理者側のルーティング設定
+  # namespace :admin do
+  #   resources :items, only: [:index, ]
+  # end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
