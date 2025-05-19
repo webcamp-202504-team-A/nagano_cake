c6575fdcd9259c04c158c4c991734652cd266710
aoi_takenaka <aaoooii219@gmail.com>
2025-05-19 06:56:00 +0000

    Add: 配送先フォーム作成

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,16 +1,30 @@
 class Public::AddressesController < ApplicationController
   # TODO: ログインしていない時にアクセスするとログイン画面へ遷移
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
 
   # 他人のアクセス防止
 
   def index
+    @new_address = Address.new
+
+    # customer = Customer.find(current_user.id)
+    # @address = customer.address
+
+    # @address = current_user.address
   end
 
   def edit
   end
 
   def create
+    @new_address = Address.new(address_params)
+    @new_address.customer_id = current_user.id
+
+    if @new_address.save
+      redirect_to request.referer
+    else
+      render "index"
+    end
   end
 
   def update
--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,16 @@
 class Address < ApplicationRecord
   belongs_to :customer
+
+  validates :name, presence: true
+  validates :postal_code,
+    presence: true,
+    format: {
+      with: /\A\d{7}\z/,
+      message: "7桁の数字で入力してください（ハイフン不要）"
+    },
+    length: {
+      is: 7,
+      message: "桁で入力してください"
+    }
+  validates :address, presence: true
 end
