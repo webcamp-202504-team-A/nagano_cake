e7213e94b231de7f1698940de3bd394fb7c73812
kkraoi <aaoooii219@gmail.com>
2025-05-21 12:55:43 +0000

    add: カートに追加する処理作成

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,11 +1,23 @@
 class Public::CartItemsController < ApplicationController
   def index
-    
+  end
+
+  def create
+    @cart_item = CartItem.new(cart_item_params)
+    @cart_item.customer_id = current_customer.id
+
+    if @cart_item.save
+      redirect_to cart_items_path
+    else
+      @item = Item.find(params[:id])
+      redirect_to request.referer
+    end
   end
 
   private
-  
+
   def cart_item_params
     params.require(:cart_item).permit(:item_id, :amount)
+    
   end
 end
--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,2 +1,7 @@
 class CartItem < ApplicationRecord
+  belongs_to :customer
+  belongs_to :item
+
+  validates :item_id, presence: true
+  validates :amount, presence: true
 end
