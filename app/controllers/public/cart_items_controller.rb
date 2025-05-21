a2702c582de378f2884ecda476019897563d6059
kkraoi <aaoooii219@gmail.com>
2025-05-21 13:57:19 +0000

    Fix: 既存のカートアイテムを考慮した処理に調整

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,18 +1,30 @@
 class Public::CartItemsController < ApplicationController
   def index
+    @cart_items = CartItem.all
+    # byebug
   end
 
   def create
-    @cart_item = CartItem.new(cart_item_params)
-    @cart_item.customer_id = current_customer.id
+    cart_item = CartItem.new(cart_item_params)
+    existing_item = current_customer.cart_items.find_by(item_id: cart_item_params[:item_id])
 
-    if @cart_item.save
-      flash[:notice] = "カートに商品を追加しました"
+    if existing_item
+      existing_item.amount += cart_item_params[:amount].to_i
+      if existing_item.save
+        flash[:notice] = "カートの数量を更新しました"
+      else
+        flash[:alert] = "更新に失敗しました"
+      end
     else
-      flash[:alert] = "追加に失敗しました"
+      cart_item.customer_id = current_customer.id
+      if cart_item.save
+        flash[:notice] = "カートに商品を追加しました"
+      else
+        flash[:alert] = "追加に失敗しました"
+      end
     end
 
-    @item = Item.find(@cart_item[:item_id])
+    @item = Item.find(cart_item[:item_id])
     redirect_to request.referer
   end
 
