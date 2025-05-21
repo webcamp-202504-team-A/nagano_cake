d760016c83dd67597dbf8cceaa21ac6c05ecb6a7
kkraoi <aaoooii219@gmail.com>
2025-05-21 11:58:54 +0000

    Add: カートのビューを作成

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,2 +1,11 @@
 class Public::CartItemsController < ApplicationController
+  def index
+    
+  end
+
+  private
+  
+  def cart_item_params
+    params.require(:cart_item).permit(:item_id, :amount)
+  end
 end
