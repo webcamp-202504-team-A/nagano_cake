177ca64735ec80700ca3f58ec34fb0c40e6d9480
monyu-01 <opsu.0102@gmail.com>
2025-05-22 07:07:15 +0000

    Add: 管理者用注文履歴一覧画面の実装（admin/homes#top）

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,4 +1,5 @@
 class Admin::HomesController < Admin::BaseController
   def top
+    @orders = Order.includes(:customer, :order_details).order(created_at: :desc).page(params[:page]).per(10)
   end
 end
---
+++ app/controllers/admin/orders_controller.rb
@@ -0,0 +1,3 @@
+class Admin::OrdersController < ApplicationController
+  
+end
