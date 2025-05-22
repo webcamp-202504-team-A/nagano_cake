cea3a9fee9c8046719aa3d372fd8f1fab37a3f3e
Alpinia-Shell <201705012+Alpinia-Shell@users.noreply.github.com>
2025-05-22 06:13:50 +0000

    Add:注文確認画面

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -9,6 +9,23 @@ class Public::OrdersController < ApplicationController
   end
 
   def confirm
+    @order = Order.new(order_params)
+    @order.customer = current_customer
+    case params[:order][:address_type]
+    when  "member_address"
+      @order.postal_code = current_customer.postal_code
+      @order.address = current_customer.address
+      @order.name = current_customer.last_name + current_customer.first_name
+    when "saved_address"
+      @address = Address.find(params[:order][:address_id])
+      @order.postal_code = @address.postal_code
+      @order.address = @address.address
+      @order.name = @address.name
+    when "new_address"
+    else
+      flash[:alert] = "住所の選択に問題があります"
+      redirect_to new_order_path
+    end
   end
 
   def complate
@@ -18,5 +35,10 @@ class Public::OrdersController < ApplicationController
   end
 
   def show
+  end
+
+  private
+  def order_params
+    params.require(:order).permit(:payment_method, :postal_code, :address, :name)
   end
 end
