bb140246ce148303c4693b186696f4ce1917eee0
Alpinia-Shell <201705012+Alpinia-Shell@users.noreply.github.com>
2025-05-22 11:59:36 +0000

    Add:アソシエーション追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
   has_one_attached :image
   belongs_to :genre
+  has_many :order_details, dependent: :destroy
 
   def price_with_tax
     (price_without_tax * 1.1).floor
