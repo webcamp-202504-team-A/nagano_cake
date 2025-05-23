40324a3942fa5fe1c7a5dcc4e0870b5bc90ce7da
monyu-01 <opsu.0102@gmail.com>
2025-05-23 06:38:53 +0000

    Add: 日本語バリデーションエラーメッセージをja.ymlに追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,9 @@
+Admin.create!(
+  email: 'admin@example.com',
+  password: 'password',  
+  password_confirmation: 'password'
+)
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
