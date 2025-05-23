1406186ee5485493c0f411a69cc56a68115054e0
Alpinia-Shell <201705012+Alpinia-Shell@users.noreply.github.com>
2025-05-23 04:40:08 +0000

    Add:会員登録バリデーション追加

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -8,11 +8,11 @@ class Customer < ApplicationRecord
   has_many :cart_items
   validates :last_name, presence: true
   validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
-  validates :postal_code, presence: true
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力してください" }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力してください" }
+  validates :postal_code, presence: true, format: { with: /\A\d{7}\z/, message: "は7桁の数字で入力してください" }
   validates :address, presence: true
-  validates :telephone_number, presence: true
+  validates :telephone_number, presence: true, format: { with: /\A\d{10,11}\z/, message: "は10〜11桁の数字で入力してください" }
   
   def total_cart_price
     cart_items.includes(:item).sum { |ci| ci.item.price_with_tax * ci.amount }
