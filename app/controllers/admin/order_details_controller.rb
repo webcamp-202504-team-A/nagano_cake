352953e8f8e7344263c36aa855381ecf337ed686
monyu-01 <opsu.0102@gmail.com>
2025-05-24 06:34:21 +0000

    Fix: 管理者用製作ステータスと注文ステータスの連動操作の訂正

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,11 +1,17 @@
 class Admin::OrderDetailsController < Admin::BaseController
   def update
     @order_detail = OrderDetail.find(params[:id])
-    if @order_detail.update(order_detail_params)
-      flash[:notice] = "製作ステータスを更新しました"
-    else
-      flash[:alert] = "更新に失敗しました"
+    @order_detail.update(production_status: params[:order_detail][:production_status])
+    order = @order_detail.order
+    if params[:order_detail][:production_status] == "in_production"
+      order.update(status:"in_production")
     end
+    
+    if is_all_order_details_finished(order)
+       order.update(status: "preparing_for_shipment")
+    end
+    
+    flash[:notice] = "更新に成功しました。"
     redirect_to admin_order_path(@order_detail.order_id)
   end
 
@@ -13,5 +19,14 @@ class Admin::OrderDetailsController < Admin::BaseController
 
   def order_detail_params
     params.require(:order_detail).permit(:production_status)
+  end
+
+  def is_all_order_details_finished(order)
+    order.order_details.each do |order_detail|
+      if order_detail.production_status != 'finished'
+        return false
+      end
+    end
+     return true
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,13 +1,13 @@
 class Order < ApplicationRecord
   belongs_to :customer
-  has_many :order_details
+  has_many :order_details, dependent: :destroy
 
   enum payment_method: { credit_card: 0, transfer: 1 }
   enum status: {
-    入金待ち: 0,
-    入金確認: 1,
-    製作中: 2,
-    発送準備中: 3,
-    発送済み: 4
-  }
+    payment_pending: 0,
+    payment_confirmed: 1,
+    in_production: 2,
+    preparing_for_shipment: 3,
+    shipped: 4
+}
 end
